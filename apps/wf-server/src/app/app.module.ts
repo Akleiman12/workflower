3109c085cc7863ea4978482f3327386b2244f40d
Asher Kleiman <asherkleiman@gmail.com>
2023-02-25 13:48:20 +0100

    added backend dependencies and initial db config

--- apps/wf-server/src/app/app.module.ts
+++ apps/wf-server/src/app/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db',
+      entities: [],
+      synchronize: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
