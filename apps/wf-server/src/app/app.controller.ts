a468f44bb9c3ef831972e78a7b1aeb944a055923
Asher Kleiman <asherkleiman@gmail.com>
2023-02-27 12:50:33 +0100

    added initial endpoint structure

--- apps/wf-server/src/app/app.controller.ts
+++ apps/wf-server/src/app/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 
 import { AppService } from './app.service';
 
@@ -6,8 +6,36 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  //Health Check
   @Get('ping')
-  getData() {
+  ping() {
     return 'pong';
+  }
+
+  // Workflow endpoints
+
+  @Get('workflows')
+  getWorkflows() {
+    // ...
+  }
+
+  @Get('workflows/:id')
+  getWorkflowById(@Param('id') id: string) {
+    // ...
+  }
+
+  @Post('workflows/create')
+  createWorkflow(@Body() body) {
+    // ...
+  }
+
+  @Put('workflows/:id')
+  updateWorkflow(@Param('id') id: string, @Body() body) {
+    // ...
+  }
+
+  @Delete('workflows/:id')
+  deleteWorkflow(@Param('id') id: string) {
+    // ...
   }
 }
