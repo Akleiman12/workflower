a1b91716b0df7fe8fe3be784715fa254fdd175e7
Asher Kleiman <asherkleiman@gmail.com>
2023-03-05 21:32:40 +0100

    Functional list with delete method

--- apps/wf-client/src/app/components/workflow-list/workflow-list.tsx
+++ apps/wf-client/src/app/components/workflow-list/workflow-list.tsx
@@ -1,37 +1,60 @@
+import { Workflow } from "@workflower/wf-shared";
 import React from "react";
+import { Link } from "react-router-dom";
 import { WorkflowService } from "../../services/workflow.service";
 
-export class WorkflowList extends React.Component<object, { workflowsList: Array<any> }> {
-    constructor(props: any) {
+export class WorkflowList extends React.Component<object, { workflowsList: Array<Workflow> }> {
+    constructor(props: object) {
         super(props);
         this.state = {
             workflowsList: []
         }
     }
 
-    renderList(list: any = []) {
+    renderList(list: Array<Workflow> = []) {
         const renderList = [];
-        for(const i of list) {
-            renderList.push((<li key={i.name}>{i.name}</li>))
+        for(const node of list) {
+            renderList.push((
+                <tr key={node.name}>
+                    <td>{node.name}</td>
+                    <td><button><Link to={`/update/${node.id}`}>Edit</Link></button><button onClick={() => this.deleteWorkflow(node.id)}>Delete</button></td>
+                </tr>
+            ));
         }
         return renderList;
     }
 
     async componentDidMount() {
-        const list: Array<any> = await WorkflowService.getWorkflows() as any;
+        const list: Array<Workflow> = await WorkflowService.getWorkflows();
         console.log('list', list)
         this.setState({
             workflowsList: list
         })
     }
 
+    async deleteWorkflow(id: number) {
+        const result = confirm('This action can\'t be undone. Are you sure you want to proceed?');
+        if (result) {
+            await WorkflowService.deleteWorkflow(id);
+            window.location.reload();
+        }
+    }
+
     render() {
         return (
             <> 
             <h2>Workflows List</h2>
-                <ul>
-                    { this.renderList(this.state.workflowsList)}
-                </ul>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Workflow</th>
+                            <th>Options</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { this.renderList(this.state.workflowsList)}
+                    </tbody>
+                </table>
             </>
         )
     }
